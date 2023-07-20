8b71511ec730a376e3b106a8aa4ea4f10b04baec
Дмитрий Степанов <stepanovworks@mail.ru>
2023-07-20 22:33:48 +0500

    Add Test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -66,9 +66,9 @@ DATABASES = {
         # бэкенд postgresql
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django_user'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'django_password'),
-        'HOST': os.getenv('DB_HOST', 'django_db'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
